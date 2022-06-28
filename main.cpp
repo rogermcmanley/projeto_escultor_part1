b7fbf6c2287a9d96b2b88b908c3f036c7ba5e274
rogermcmanley <104937013+rogermcmanley@users.noreply.github.com>
2022-06-28 11:09:35 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,285 @@
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "sculptor.h"
+
+
+using namespace std;
+
+int main(){
+  //Testando Esfera e Elipsoide 
+  Sculptor testes(60,50,50);
+    //Esfera  
+    testes.setColor(1,0,0,1);
+    testes.putSphere(30,30,30,10);
+    testes.cutSphere(30,30,30,5);
+
+    //Elipsoide
+    testes.setColor(0,1,0,1);
+    testes.putEllipsoid(20,20,20,15,10,7);
+    testes.writeOFF((char*)"Testes.off");
+
+
+
+  //Escultura principal 
+  Sculptor porco(50,50,50);
+  
+  //corpo
+  porco.setColor(1,0.7,0.75,1); 
+  porco.putBox(18,33,5,13,3,13);
+  //cabeca
+  porco.setColor(1,0.7,0.75,1);
+  porco.putBox(31,39,7,15,4,12);
+  //lama na cabeca
+  porco.setColor(0.5,0.27,0.075,1); 
+  porco.putBox(34,38,15,16,4,9);
+  porco.cutBox(33,39,14,15,4,8);
+  porco.setColor(0.72,0.44,0.24,1); 
+  porco.putBox(33,39,14,15,4,8);
+  porco.cutBox(36,37,14,15,3,4);
+  porco.setColor(0.72,0.44,0.24,1); 
+  porco.putBox(36,37,14,15,3,4);
+  porco.cutBox(37,38,13,14,4,5);
+  porco.setColor(0.78,0.50,0.31,1); 
+  porco.putBox(37,38,13,14,4,5);
+  porco.cutBox(35,36,13,14,4,5);
+  porco.setColor(0.78,0.50,0.31,1); 
+  porco.putBox(35,36,13,14,4,5);
+  //rosa na cabeca
+  porco.setColor(0.19,0.8,0.19,1);
+  porco.putBox(36,37,16,18,4,7);
+  porco.setColor(0.19,0.8,0.19,1);
+  porco.putBox(36,37,17,18,3,4);
+  porco.setColor(0.19,0.8,0.19,1);
+  porco.putBox(36,37,18,19,5,6);
+  porco.setColor(1,0,0,1);
+  porco.putBox(35,38,19,21,5,6);
+  porco.setColor(1,0,0,1);
+  porco.putBox(36,37,19,22,4,7);
+  porco.cutBox(36,37,21,22,5,6);
+  //focinho
+  porco.setColor(0.94,0.5,0.5,1);
+  porco.putBox(39,40,9,12,5,11);
+  //perna ant. esq.
+  porco.setColor(0.5,0.27,0.075,1); 
+  porco.putBox(30,33,2,5,3,6);
+   //perna ant. dir.
+  porco.setColor(0.5,0.27,0.075,1); 
+  porco.putBox(30,33,2,5,10,13);
+  //perna post. esq.
+  porco.setColor(0.5,0.27,0.075,1); 
+  porco.putBox(18,21,2,5,3,6);
+  //perna post. dir.
+  porco.setColor(0.5,0.27,0.075,1); 
+  porco.putBox(18,21,2,5,10,13);
+  //rabo
+  porco.setColor(1,0.7,0.75,1); 
+  porco.putBox(15,18,9,10,7,9);
+  porco.setColor(1,0.7,0.75,1); 
+  porco.putBox(15,16,9,11,7,9);
+  porco.setColor(1,0.7,0.75,1); 
+  porco.putBox(15,17,11,12,7,9);
+  //olho direito
+  porco.cutBox(38,39,12,1,10,13); 
+  porco.setColor(1,1,1,1);
+  porco.putBox(38,39,12,13,10,12);
+  porco.setColor(0,0,0,1);
+  porco.putBox(38,39,12,13,11,12);
+  //olho esquerdo
+  porco.cutBox(38,39,12,1,4,7); 
+  porco.setColor(0,0,0,1);
+  porco.putBox(38,39,12,13,4,6);
+  porco.setColor(1,1,1,1);
+  porco.putBox(38,39,12,13,5,6);
+  //nariz 
+  porco.cutBox(39,40,10,11,6,7); 
+  porco.setColor(0.9,0.35,0.35,1);
+  porco.putBox(39,40,10,11,6,7);
+  porco.cutBox(39,40,10,11,9,10); 
+  porco.setColor(0.9,0.35,0.35,1);
+  porco.putBox(39,40,10,11,9,10);
+  //orelhas
+  porco.setColor(0.94,0.5,0.5,1);
+  porco.putBox(36,37,12,16,10,13);
+  porco.setColor(0.94,0.5,0.5,1);
+  porco.putBox(36,37,12,14,3,6); 
+  porco.setColor(0.5,0.27,0.075,1);
+  porco.putVoxel(36,15,3);
+  //lama
+  porco.setColor(0.5,0.27,0.075,1); 
+  porco.putBox(15,36,0,2,0,16);
+  porco.cutBox(18,33,5,6,3,13);
+  porco.setColor(0.5,0.27,0.075,1); 
+  porco.putBox(18,33,5,6,3,13);
+  porco.cutBox(18,33,6,7,3,13);
+  porco.setColor(0.65,0.36,0.16,1); 
+  porco.putBox(18,33,6,7,3,13);
+  porco.cutBox(33,39,7,8,4,12);
+  porco.setColor(0.72,0.44,0.24,1); 
+  porco.putBox(33,39,7,8,4,12);
+  porco.cutBox(28,33,7,8,3,13);
+  porco.setColor(0.72,0.44,0.24,1); 
+  porco.putBox(28,33,7,8,3,13);
+  porco.cutBox(18,21,7,8,3,13);
+  porco.setColor(0.72,0.44,0.24,1); 
+  porco.putBox(18,21,7,8,3,13);
+  porco.cutBox(18,20,8,9,3,13);
+  porco.setColor(0.78,0.50,0.31,1); 
+  porco.putBox(18,20,8,9,3,13);
+  porco.cutBox(18,19,9,10,3,13);
+  porco.setColor(0.78,0.50,0.31,1); 
+  porco.putBox(18,19,9,10,3,13);
+  porco.cutBox(15,18,9,10,7,9);
+  porco.setColor(0.78,0.50,0.31,1); 
+  porco.putBox(15,18,9,10,7,9);
+  porco.cutBox(23,26,7,8,3,13);
+  porco.setColor(0.72,0.44,0.24,1); 
+  porco.putBox(23,26,7,8,3,13);
+  porco.cutBox(24,25,8,9,3,13);
+  porco.setColor(0.78,0.50,0.31,1); 
+  porco.putBox(24,25,8,9,3,13);
+  porco.cutBox(29,32,8,9,3,13);
+  porco.setColor(0.78,0.50,0.31,1); 
+  porco.putBox(29,32,8,9,3,13);
+  porco.cutBox(30,31,9,10,3,13);
+  porco.setColor(0.78,0.50,0.31,1); 
+  porco.putBox(30,31,9,10,3,13);
+  porco.cutBox(27,28,10,11,3,4);
+  porco.setColor(0.78,0.50,0.31,1); 
+  porco.putBox(27,28,10,11,3,4);
+  porco.cutBox(19,20,11,12,12,13);
+  porco.setColor(0.78,0.50,0.31,1); 
+  porco.putBox(19,20,11,12,12,13);
+  
+  porco.setColor(0.65,0.36,0.16,1);
+  porco.putVoxel(26,5,3);
+  porco.setColor(0.65,0.36,0.16,1);
+  porco.putVoxel(23,5,3);
+
+  //cabeça homem aranha
+  porco.setColor(1,0,0,1); 
+  porco.putBox(17,25,22,29,4,12);
+  porco.cutBox(24,25,25,26,7,9);
+  porco.setColor(0,0,0,1); 
+  porco.putBox(24,25,25,26,7,9);
+  //corpo homem aranha
+  porco.setColor(1,0,0,1); 
+  porco.putBox(19,22,13,22,4,12);
+  porco.cutBox(21,22,17,20,7,9);
+  porco.setColor(0,0,0,1); 
+  porco.putBox(21,22,17,20,7,9);
+  // aranha peito
+  porco.cutVoxel(21,18,9);
+  porco.setColor(0,0,0,1);
+  porco.putVoxel(21,18,9);
+  porco.cutVoxel(21,20,9);
+  porco.setColor(0,0,0,1);
+  porco.putVoxel(21,20,9);
+  porco.cutVoxel(21,18,6);
+  porco.setColor(0,0,0,1);
+  porco.putVoxel(21,18,6);
+  porco.cutVoxel(21,20,6);
+  porco.setColor(0,0,0,1);
+  porco.putVoxel(21,20,6);
+  //olho esquerdo
+  porco.cutBox(24,25,25,26,5,7);
+  porco.setColor(1,1,1,1); 
+  porco.putBox(24,25,25,26,5,7);
+  porco.cutVoxel(25,27,5);
+  porco.setColor(1,1,1,1); 
+  porco.putVoxel(24,26,5);
+  porco.cutVoxel(25,27,5);
+  porco.setColor(0,0,0,1); 
+  porco.putVoxel(24,26,6);
+  porco.cutBox(24,25,24,25,5,7);
+  porco.setColor(0,0,0,1); 
+  porco.putBox(24,25,24,25,5,7);
+  porco.cutBox(24,25,25,27,4,5);
+  porco.setColor(0,0,0,1); 
+  porco.putBox(24,25,25,27,4,5);
+  porco.cutBox(24,25,27,28,4,6);
+  porco.setColor(0,0,0,1); 
+  porco.putBox(24,25,27,28,4,6);
+  
+  //olho direito
+  porco.cutBox(24,25,25,26,9,11);
+  porco.setColor(1,1,1,1); 
+  porco.putBox(24,25,25,26,9,11);
+  porco.cutVoxel(25,27,5);
+  porco.setColor(1,1,1,1); 
+  porco.putVoxel(24,26,10);
+  porco.cutVoxel(25,27,9);
+  porco.setColor(0,0,0,1); 
+  porco.putVoxel(24,26,9);
+  porco.cutBox(24,25,24,25,9,11);
+  porco.setColor(0,0,0,1); 
+  porco.putBox(24,25,24,25,9,11);
+  porco.cutBox(24,25,25,27,11,12);
+  porco.setColor(0,0,0,1); 
+  porco.putBox(24,25,25,27,11,12);
+  porco.cutBox(24,25,27,28,10,12);
+  porco.setColor(0,0,0,1); 
+  porco.putBox(24,25,27,28,10,12);
+  //roupa
+  porco.cutBox(19,22,13,16,7,9);
+  porco.setColor(0,0,1,1); 
+  porco.putBox(19,22,13,16,7,9);
+
+  
+  //roupa esquerda
+  porco.cutBox(19,22,13,20,4,5);
+  porco.setColor(0,0,1,1); 
+  porco.putBox(19,22,13,20,4,5);
+  porco.cutBox(19,22,13,19,5,6);
+  porco.setColor(0,0,1,1); 
+  porco.putBox(19,22,13,19,5,6);
+  porco.cutBox(19,22,13,17,6,7);
+  porco.setColor(0,0,1,1); 
+  porco.putBox(19,22,13,17,6,7);
+  //roupa direita
+  porco.cutBox(19,22,13,20,11,12);
+  porco.setColor(0,0,1,1); 
+  porco.putBox(19,22,13,20,11,12);
+  porco.cutBox(19,22,13,19,10,11);
+  porco.setColor(0,0,1,1); 
+  porco.putBox(19,22,13,19,10,11);
+  porco.cutBox(19,22,13,17,9,10);
+  porco.setColor(0,0,1,1); 
+  porco.putBox(19,22,13,17,9,10);
+  //perna esquerda homem aranha
+  porco.setColor(0,0,1,1); 
+  porco.putBox(22,26,13,16,4,7);
+  //perna direita homem aranha
+  porco.setColor(0,0,1,1); 
+  porco.putBox(22,26,13,16,9,12);
+  //bota esquerda homem aranha
+  porco.setColor(1,0,0,1); 
+  porco.putBox(26,31,13,16,4,7);
+  porco.cutBox(27,28,13,16,4,7);
+  porco.setColor(0,0,0,1); 
+  porco.putBox(27,28,13,16,4,7);
+  //bota direita homem aranha
+  porco.setColor(1,0,0,1); 
+  porco.putBox(26,31,13,16,9,12);
+  porco.cutBox(27,28,13,16,9,12);
+  porco.setColor(0,0,0,1); 
+  porco.putBox(27,28,13,16,9,12);
+  //braço esquerdo 
+  porco.setColor(1,0,0,1); 
+  porco.putBox(19,22,13,22,1,4);
+  porco.cutBox(19,22,15,16,1,4);
+  porco.setColor(0,0,0,1);
+  porco.putBox(19,22,15,16,1,4);
+  //braço esquerdo 
+  porco.setColor(1,0,0,1); 
+  porco.putBox(19,22,13,22,12,15);
+  porco.cutBox(19,22,15,16,12,15);
+  porco.setColor(0,0,0,1);
+  porco.putBox(19,22,15,16,12,15);
+
+  
+  porco.writeOFF("porco.off");
+
+  return 0;
+}
---
+++ sculptor.cpp
@@ -0,0 +1,243 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include "sculptor.h"
+#include <iomanip>
+#include <queue>
+#include <cmath>
+#include <fstream>
+
+using namespace std;
+
+  Sculptor::Sculptor(int _nx, int _ny, int _nz){
+
+  nx = _nx;
+  ny = _ny;
+  nz = _nz;
+  fix = 0.5;
+  r = 0.5;
+  g = 0.5; 
+  b = 0.5;
+  a = 0.5; 
+  
+   //alocacao da matriz
+  v = new Voxel**[nx];
+    for (int i =0; i<nx; i++){
+    v[i] = new Voxel*[ny];
+
+    for (int j =0; j<ny; j++){
+      v[i][j]= new Voxel[nz];
+    }
+  }
+}
+
+  Sculptor:: ~Sculptor(){
+    for(int i=0; i<nx; i++){
+        for (int j=0; j<ny; j++){
+               delete[] v[i][j];
+            }
+        }
+        for (int i=0; i <nx; i++){
+            delete[] v[i];
+        }
+
+        delete[] v;
+    
+        nx = 0;
+        ny = 0;
+        nz = 0;
+    
+    }
+   
+  void Sculptor::setColor(float _r, float _g, float _b, float _a){
+    r = _r;
+    g = _g;
+    b = _b;
+    a = _a;
+}
+  void Sculptor::putVoxel(int x, int y, int z){
+    v[x][y][z].isOn = true;
+    v[x][y][z].r = r;
+    v[x][y][z].g = g;
+    v[x][y][z].b = b;
+    v[x][y][z].a = a;
+}
+  void Sculptor::cutVoxel(int x, int y, int z){
+    v[x][y][z].isOn = false;
+    
+}
+void Sculptor::putBox(int x0_, int x1_, int y0_, int y1_, int z0_, int z1_){
+    for(int x = x0_; x<x1_; x++){
+       for (int y = y0_; y<y1_; y++){
+           for (int z = z0_; z<z1_; z++){
+               v[x][y][z].isOn=true;
+               v[x][y][z].r = r;
+               v[x][y][z].g = g;
+               v[x][y][z].b = b;
+               v[x][y][z].a = a;
+           }
+       }
+    }
+    
+}
+void Sculptor::cutBox(int x0, int x1, int y0, int y1, int z0, int z1)
+{
+    for(int x = x0; x<x1; x++){
+       for (int y = y0; y<y1; y++){
+           for (int z = z0; z<z1; z++){
+               v[x][y][z].isOn = false;
+           }
+       }
+    }
+}
+void Sculptor::putEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz){
+   float newx, newy, newz;
+
+    for (int x = 0; x < nx; x++){
+        for (int y = 0; y < ny; y++){
+            for (int z = 0; z < nz; z++){
+            newx = ((float)(x-xcenter)*(float)(x-xcenter))/(rx * rx);
+            newy = ((float)(y-ycenter)*(float)(y-ycenter))/(ry * ry);
+            newz = ((float)(z-zcenter)*(float)(z-zcenter))/(rz * rz);
+
+            if ((newx + newy + newz) < 1){
+                putVoxel(x,y,z);
+            }
+            }
+        }
+    }
+}
+
+void Sculptor::cutEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz){
+   double newx, newy, newz;
+
+    for (int x = 0; x < nx; x++){
+        for (int y = 0; y < ny; y++){
+            for (int z = 0; z < nz; z++){
+            newx = ((float)(x-xcenter)*(float)(x-xcenter))/(rx * rx);
+            newy = ((float)(y-ycenter)*(float)(y-ycenter))/(ry * ry);
+            newz = ((float)(z-zcenter)*(float)(z-zcenter))/(rz * rz);
+
+            if ((newx + newy + newz) < 1){
+                cutVoxel(x,y,z);
+            }
+            }
+        }
+    }
+}
+void Sculptor::putSphere(int xcenter, int ycenter, int zcenter, int radius){
+  for(int i = -radius; i<=radius; i++){
+      for(int j = -radius; j<=radius; j++){
+          for(int k = -radius; k<=radius; k++){
+         
+            if ((i*i+j*j+k*k) < radius*radius){
+              putVoxel(i+xcenter,j+ycenter,k+zcenter);
+            }
+          }
+      }
+  }
+
+}
+
+void Sculptor::cutSphere(int xcenter, int ycenter, int zcenter, int radius){
+  for(int i = -radius; i<radius; i++){
+      for(int j = -radius; j<radius; j++){
+          for(int k = -radius; k<radius; k++){
+            if((i*i+j*j+k*k) < radius*radius){
+              cutVoxel(i+xcenter,j+ycenter,k+zcenter);
+            }
+          }
+      }
+  }
+}
+  void Sculptor::writeOFF(char *filename)
+{
+  std::ofstream fout;
+  int nvertices, nfaces;
+  fout.open("porco.off");
+  if(!fout.is_open()){
+    std::cout << "deu errado\n";
+    exit(1);
+  }
+  nvertices = 8;
+  nfaces = 6;
+  fout << "OFF\n";
+  fout << nvertices << " " << nfaces << " 0\n";
+  fout.close();
+{
+  int quantvox = 0;
+  int ref;
+  fout.open(filename);
+
+  fout<<"OFF\n";
+
+  
+  for (int i = 0; i < nx; i++){
+      for (int j = 0; j <ny; j++){
+           for (int k = 0; k <nz; k++){
+                if(v[i][j][k].isOn == true){
+                quantvox++;
+                }
+           }
+       }
+  }
+  fout<<quantvox * 8<<" "<<quantvox * 6 << " " << "0" << "\n"; 
+
+  for (int a = 0; a < nx; a++){
+      for (int b = 0; b < ny; b++){
+           for (int c = 0; c < nz; c++){
+                if(v[a][b][c].isOn == true){
+                fout << a - fix << " " << b + fix << " " << c - fix << "\n" << flush;
+                fout << a - fix << " " << b - fix << " " << c - fix << "\n" << flush;
+                fout << a + fix << " " << b - fix << " " << c - fix << "\n" << flush;
+                fout << a + fix << " " << b + fix << " " << c - fix << "\n" << flush;
+                fout << a - fix << " " << b + fix << " " << c + fix << "\n" << flush;
+                fout << a - fix << " " << b - fix << " " << c + fix << "\n" << flush;
+                fout << a + fix << " " << b - fix << " " << c + fix << "\n" << flush;
+                fout << a + fix << " " << b + fix << " " << c + fix << "\n" << flush;
+                }
+           }
+      }
+  }
+
+  quantvox = 0;
+
+  
+  for (int a= 0; a<nx; a++){
+      for (int b = 0; b<ny; b++){
+           for (int c= 0; c<nz; c++){
+                if(v[a][b][c].isOn == true){
+                ref = quantvox * 8;
+                fout << fixed;
+
+                // montar linha que realiza a construcao das faces a partir do vertices e mostrar as propriedades rgba do voxel
+                fout << "4" << " " << 0+ref << " " << 3+ref << " " << 2+ref << " " << 1+ref << " ";
+                fout << setprecision(2)<<v[a][b][c].r << " " << setprecision(2)<<v[a][b][c].g <<" " << setprecision(2)<<v[a][b][c].b << " " << setprecision(2) << v[a][b][c].a << "\n";
+
+                fout << "4" << " " << 4+ref << " " << 5+ref << " " << 6+ref << " " << 7+ref << " ";
+                fout << setprecision(2)<<v[a][b][c].r << " " << setprecision(2)<<v[a][b][c].g <<" " << setprecision(2)<<v[a][b][c].b << " " << setprecision(2) << v[a][b][c].a << "\n";
+
+                fout << "4" << " " << 0+ref << " " << 1+ref << " " << 5+ref << " " << 4+ref << " ";
+                fout << setprecision(2)<<v[a][b][c].r << " " << setprecision(2)<<v[a][b][c].g <<" " << setprecision(2)<<v[a][b][c].b << " " << setprecision(2) << v[a][b][c].a << "\n";
+
+                fout << "4" << " " << 0+ref << " " << 4+ref << " " << 7+ref << " " << 3+ref << " ";
+                fout << setprecision(2)<<v[a][b][c].r << " " << setprecision(2)<<v[a][b][c].g <<" " << setprecision(2)<<v[a][b][c].b << " " << setprecision(2) << v[a][b][c].a << "\n";
+
+                fout << "4" << " " << 3+ref << " " << 7+ref << " " << 6+ref << " " << 2+ref << " ";
+                fout << setprecision(2)<<v[a][b][c].r << " " << setprecision(2)<<v[a][b][c].g <<" " << setprecision(2)<<v[a][b][c].b << " " << setprecision(2) << v[a][b][c].a << "\n";
+
+                fout << "4" << " " << 1+ref << " " << 2+ref << " " << 6+ref << " " << 5+ref << " ";
+                fout << setprecision(2)<<v[a][b][c].r << " " << setprecision(2)<<v[a][b][c].g <<" " << setprecision(2)<<v[a][b][c].b << " " << setprecision(2) << v[a][b][c].a << "\n";
+
+                quantvox = quantvox + 1;
+                }
+           }
+       }
+  }
+  fout.close();
+}
+}
+
+
+
+
